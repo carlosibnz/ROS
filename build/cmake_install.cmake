14d5b22d878ac19c7ce27507ab9016e03339a7fb
igorrecioh <igor.recio.h@gmail.com>
2018-02-18 19:37:45 +0100

    beginner_tutorials package deleted

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,7 +117,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/igor/catkin_ws/build/gtest/cmake_install.cmake")
-  include("/home/igor/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
   include("/home/igor/catkin_ws/build/geek_gasteiz/cmake_install.cmake")
 
 endif()
